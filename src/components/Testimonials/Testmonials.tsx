299f8620c013bdf9f206ce4f62099d36ac81d40a
crishard <crislantorres9@gmail.com>
2024-10-08 20:52:48 -0300

    feature: criando seção testimonials

---
+++ src/components/Testimonials/Testmonials.tsx
@@ -0,0 +1,9 @@
+import { TestimonialCarousel } from "./TestimonialCarousel"
+
+export const Testimonials = () => {
+  return (
+    <div className="px-24 max-[600px]:px-10 ">
+      <TestimonialCarousel />
+    </div>
+  )
+}
