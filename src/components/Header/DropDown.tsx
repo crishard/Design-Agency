ff04180719b1921f63099cf5753cbd1eebb89110
crishard <crislantorres9@gmail.com>
2024-10-08 20:47:40 -0300

    feature: ajuste de estilos

--- src/components/Header/DropDown.tsx
+++ src/components/Header/DropDown.tsx
@@ -9,7 +9,7 @@ export const DropDown = ({ isOpen, servicesMenuRef }: IDropDownProps) => {
     return (
         <div>
             {isOpen && (
-                <div className="absolute top-20" ref={servicesMenuRef}>
+                <div className="absolute top-8" ref={servicesMenuRef}>
                     <li className="border-b-[2px] border-gray-200 bg-white py-2 px-4">
                         <a href="#services" className="flex items-center gap-12">
                             Level 1 item 1 <FaChevronRight className="text-bg-green" />
