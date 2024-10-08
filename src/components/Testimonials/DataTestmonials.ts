a83a52338d1f13ac8f9563094e319908e1743b34
crishard <crislantorres9@gmail.com>
2024-10-08 20:51:19 -0300

    feature: criando dados de testmonials

---
+++ src/components/Testimonials/DataTestmonials.ts
@@ -0,0 +1,39 @@
+export interface DataTestimonial {
+    message: string;
+    name: string;
+    title: string;
+    image: string;
+}
+
+export const dataTestimonials: DataTestimonial[] = [
+    {
+        message: 'Thank You for your service. I am very pleased with the result. I have seen exponencial growth in my business and its all thanks to your amazing service',
+        name: 'Emily Stones',
+        title: 'CEO, Marketing Guru',
+        image: 'https://randomuser.me/api/portraits/women/1.jpg',
+    },
+    {
+        message: 'Thank You for your service. I am very pleased with the result. I have seen exponencial growth in my business and its all thanks to your amazing service',
+        name: 'John Doe',
+        title: 'CTO, Tech Company',
+        image: 'https://randomuser.me/api/portraits/men/2.jpg',
+    },
+    {
+        message: 'Thank You for your service. I am very pleased with the result. I have seen exponencial growth in my business and its all thanks to your amazing service',
+        name: 'Jane Smith',
+        title: 'Founder, Startup Inc.',
+        image: 'https://randomuser.me/api/portraits/women/3.jpg',
+    },
+    {
+        message: 'Thank You for your service. I am very pleased with the result. I have seen exponencial growth in my business and its all thanks to your amazing service',
+        name: 'Michael Brown',
+        title: 'Designer, Creative Agency',
+        image: 'https://randomuser.me/api/portraits/men/4.jpg',
+    },
+    {
+        message: 'Thank You for your service. I am very pleased with the result. I have seen exponencial growth in my business and its all thanks to your amazing service',
+        name: 'Sara Williams',
+        title: 'CEO, HealthTech',
+        image: 'https://randomuser.me/api/portraits/women/5.jpg',
+    },
+];
