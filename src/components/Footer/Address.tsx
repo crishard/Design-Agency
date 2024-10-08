21632187b216a0842cfc010fe7e3c15427b3b70a
crishard <crislantorres9@gmail.com>
2024-10-08 20:37:44 -0300

    feature: criando informações de endereço do footer

---
+++ src/components/Footer/Address.tsx
@@ -0,0 +1,15 @@
+
+export const Address = () => {
+    return (
+        <div>
+            <h5 className="font-semibold text-xl pb-5">Address</h5>
+            <div >
+                <p className="block text-lg">Design Agency Head Office.</p>
+                <p className="block text-lg">Airport Road</p>
+                <p className="block text-lg">United Arab Emirate</p>
+
+            </div>
+
+        </div>
+    )
+}
