b90733f303224d35263ed5d5c67c47185b5c3aea
crishard <crislantorres9@gmail.com>
2024-10-08 20:53:32 -0300

    feature: criando card de serviços

---
+++ src/components/What we do/CardWhatWeDo.tsx
@@ -0,0 +1,19 @@
+import { FaArrowRightLong } from "react-icons/fa6";
+
+
+interface ICardWhatWeDoProps {
+    title: string
+    img: string
+}
+
+export const CardWhatWeDo = ({ title, img }: ICardWhatWeDoProps) => {
+    return (
+        <div className="hover:shadow-md px-6 py-8 rounded-xl max-w-[350px]">
+            <img src={img} alt="What We Do" className="w-20 pb-5"/>
+            <h3 className="text-xl font-semibold pb-4">{title}</h3>
+            <p>We help identify the best ways to improve tour business</p>
+
+            <button className="flex items-center gap-3 hover:text-white px-4 py-4 mt-6 rounded-xl cursor-pointer hover:bg-orange">Learn More<FaArrowRightLong /></button>
+        </div>
+    )
+}
