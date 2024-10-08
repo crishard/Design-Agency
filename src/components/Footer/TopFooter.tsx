89c6418207c34c7622283fbfed912c22f448baef
crishard <crislantorres9@gmail.com>
2024-10-08 20:36:53 -0300

    feature: criando informações de logo no footer

---
+++ src/components/Footer/TopFooter.tsx
@@ -0,0 +1,9 @@
+
+export const TopFooter = () => {
+    return (
+        <div>
+            <h1 className="font-bold text-5xl pb-6">WEB LOGO</h1>
+            <p className="text-xl">Some footer text about the Agency. Just a little description to help people understand you better</p>
+        </div>
+    )
+}
