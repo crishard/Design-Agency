b065a5c38abb2c4ed66c2907c1b96e0c0bcf5882
crishard <crislantorres9@gmail.com>
2024-10-08 20:37:24 -0300

    feature: criando links do footer

---
+++ src/components/Footer/QuickLinks.tsx
@@ -0,0 +1,15 @@
+
+export const QuickLinks = () => {
+  return (
+    <div>
+      <h5 className="font-semibold text-xl pb-1">Quick Links</h5>
+      <div >
+        <a href="/#services" className="block py-2 text-lg">Services</a>
+        <a href="/#portfolio" className="block py-2 text-lg">Portfolio</a>
+        <a href="/#about" className="block py-2 text-lg">About Us</a>
+        <a href="/#contact" className="block py-2 text-lg">Contact Us</a>
+      </div>
+
+    </div>
+  )
+}
