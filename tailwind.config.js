1d3c67a7ad1cb6af6236edfcc6afd2430da16370
crishard <crislantorres9@gmail.com>
2024-10-08 16:38:58 -0300

    configurando cores do site

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,9 @@ export default {
         "white": "#FFFFFF",
         "orange": "#F55F1D",
         "bg-secondary": "#D7F5DC",
-        "bg-green": "#20B15A"
+        "bg-green": "#06FB68",
+        "bg-primary": "#F8FFF9",
+        "green-variant": "#20B15A"
       }
     },
   },
